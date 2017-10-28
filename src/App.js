9cf7eb8cd78f6cf2723f43e2051bca69441939b5
Govind Nair <govindnair@Govinds-Mac-mini.local>
2017-10-28 16:52:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import ListContacts from './listContacts.js'
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+];
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ListContacts contacts={contacts}/>
+      </div>
+    );
+  }
+}
+
+export default App;
